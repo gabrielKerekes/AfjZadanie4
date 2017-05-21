8cfb7b31389a244ed99e2e290bbb2e703788a2a0
Gabriel Kerekeš <gabriel.kerekes.ml@hotmail.com>
2017-05-21 11:01:04 +0200

    Main edited to final version. Tests removed from project.

--- AfjZadanie4/Program.cs
+++ AfjZadanie4/Program.cs
@@ -9,34 +9,28 @@ namespace AfjZadanie4
     {
         public static void Main(string[] args)
         {
-            //if (args.Length == 0)
-            //{
-            //    System.Console.WriteLine("Grammar filename needed as argument.");
-            //    return;
-            //}
-
-            //var fileName = args[0];
-
-            var fileNames = new List<string> { "testy\\gramatika1.txt", "testy\\gramatika2.txt", "testy\\gramatika3.txt", "testy\\gramatika4.txt", "testy\\gramatika5.txt", };
-            foreach (var fileName in fileNames)
+            if (args.Length == 0)
             {
-                try
-                {
-                    var grammar = GrammarReader.ReadGrammarFromFile(fileName);
-                    LR0AutomataCreator.TryCreateAutomata(grammar);
-                }
-                catch (RedRedConflictException)
-                {
-                    Console.WriteLine("NIE - konflikt REDUKCIA/REDUKCIA");
-                    continue;
-                }
-                catch (PresunRedConflictException)
-                {
-                    Console.WriteLine("NIE - konflikt PRESUN/REDUKCIA");
-                    continue;
-                }
+                Console.WriteLine("Grammar filename needed as argument.");
+                return;
+            }
+
+            var fileName = args[0];
+
+            try
+            {
+                var grammar = GrammarReader.ReadGrammarFromFile(fileName);
+                LR0AutomataCreator.TryCreateAutomata(grammar);
 
                 Console.WriteLine("ANO");
+            }
+            catch (RedRedConflictException)
+            {
+                Console.WriteLine("NIE - konflikt REDUKCIA/REDUKCIA");
+            }
+            catch (PresunRedConflictException)
+            {
+                Console.WriteLine("NIE - konflikt PRESUN/REDUKCIA");
             }
         }
     }
